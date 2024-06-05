54307433bbb3b6de1b489cfa7476ae18593af260
quentin delos <q.delos@P210.sn>
2024-06-05 11:39:49 +0200

    add files and folders

---
+++ ressources/pages/index.php
@@ -0,0 +1,28 @@
+<?php
+    
+?>
+
+<!DOCTYPE html>
+<html lang="fr_FR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="lib/pictures/favicon/favicon.ico" type="image/x-icon">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <?php
+            include('../../lib/includes/header.html');
+        ?>
+    </header>
+
+    <main></main>
+    
+    <footer>
+        <?php
+            include('../../lib/includes/footer.html');
+        ?>
+    </footer>
+</body>
+</html>
